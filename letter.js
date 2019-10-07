4b7cd5bc1cabe25f14aa56dd714e4f61622ce2ea
Kelly Jiang <kllyjiang@gmail.com>
2019-10-07 07:11:38 -0500

    Implementing letter.js

--- letter.js
+++ letter.js
@@ -1,1 +1,12 @@
 function Letter(value) {
+    this.value = value;
+    this.guessed = false;
+    this.display = function() {
+        return this.guessed? this.value: "_";
+    }
+    this.check = function(guess) {
+        if(this.value === guess) {
+            this.guessed = true;
+        }
+    }
+}
