71391ac5c545c458ac6f5025db7e343297b60080
Kelly Jiang <kllyjiang@gmail.com>
2019-10-07 08:06:32 -0500

    Implementing the userInput function. This function displays the current state of the game, then prompts the user to guess a letter.

--- index.js
+++ index.js
@@ -36,6 +36,53 @@ let currentWord = new Word(pickRandomWord());
 
 let guesses = 5;
 const lettersGuessed = [];
+function userInput() {
+    console.log(currentWord.display());
+    console.log(`You have ${guesses} guesses remaining`);
+    console.log(`Letters guessed: ${lettersGuessed}`);
+    inquirer.prompt([
+        {
+            message: "Guess a letter",
+            name: "userGuess"
+        }
+    ]).then(response => {
+        console.log("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+        const guess = response.userGuess;
+        if(guess > 1 || !"abcdefghijklmnopqrstuvwxyz".includes(guess.toLowerCase())) {
+            console.log("Your guess must be a letter");
+            userInput();
+        }
+        else if(lettersGuessed.includes(guess)) {
+            console.log("You have already guessed that letter");
+            userInput();
+        }
+        else {
+            lettersGuessed.push(guess);       
+            if(currentWord.check(guess)) {
+                console.log("Your guess was correct");
+                if(currentWord.solved()) {
+                    console.log("You won!");
+                    playAgain();
+                }
+                else {
+                    userInput();
+                }        
+            }
+            else {
+                guesses--;
+                console.log("Your guess was incorrect");
+                if(guesses < 1) {
+                    console.log("Game over");
+                    playAgain();
+                }
+                else {
+                    userInput();
+                }
+            }
+        }
+    })
+}
+userInput();
 
 function playAgain() {
     inquirer.prompt([
